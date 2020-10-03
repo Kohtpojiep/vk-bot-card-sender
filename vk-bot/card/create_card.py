e0517b3d35f61ea17a505344b283f88b75e4ed0c
Kohtpojiep <41484721+Kohtpojiep@users.noreply.github.com>
2020-10-03 06:54:51 +0300

    Updating Card
    Just changed card for sending...

--- vk-bot/card/create_card.py
+++ vk-bot/card/create_card.py
@@ -1,45 +1,40 @@
 from PIL import Image, ImageColor, ImageDraw, ImageFont
 import os
 
+
 path = os.path.abspath(__file__)
-file_link = path[0:-14] + 'outfile.png'
+directory = path[0:-14]
 
 
-def create(name, surname, usr_id, dataStart, dataEnd, directory):
-    img = Image.open(directory + 'avatar.jpg', 'r') #Определяем путь для аватара
-    bg = Image.open(directory + 'background.png', 'r') #Определяем путь к фону
-    background = Image.new('RGB', (710, 386), '#ffffff') #Создаем холст для работы
-    offset = (45, 39) #Оффсет аватарки пользователя
+def create(name, surname, usr_id):
+    bg = Image.open(directory + 'background.jpg', 'r') #Определяем путь к фону
+    background = Image.new('RGB', (793, 559), '#ffffff') #Создаем холст для работы
 
-    #Вставляем на холст фоновое изображение и аватар пользователя
+    #Вставляем на холст фоновое изображение
     background.paste(bg)
-    background.paste(img, offset)
 
     #Определяем путь и размер шрифта для текста
-    smallFont=ImageFont.truetype(path[0:-14] + 'Infinity.ttf',16)
-    bigFont=ImageFont.truetype(path[0:-14] + 'Infinity.ttf',30)
+    smallFont=ImageFont.truetype(directory + 'Infinity.ttf', 20)
+    bigFont=ImageFont.truetype(directory + 'AcquestScript.ttf', 60)
+    
     #Создаем метод для надписей
     texting=ImageDraw.Draw(background)
 
-    x = 295 #Оффсет для текста
-    #Устанавливаем заголовок карточки
-    texting.text((x + 5, 40), 'Сущность/Организация', font=bigFont, fill = '#000000')
-    #Устанавливаем заголовки для полей
-    texting.text((x, 85), 'Имя', font=smallFont, fill = '#000000')
-    texting.text((x, 140), 'Фамилия', font=smallFont, fill = '#000000')
-    texting.text((x, 197), 'Id Вк', font=smallFont, fill = '#000000')
-    #Устанавливаем данные под заголовками
-    texting.text((x, 104), name, font=smallFont, fill = '#000000')
-    texting.text((x, 158), surname, font=smallFont, fill = '#000000')
-    texting.text((x, 218), usr_id, font=smallFont, fill = '#000000')
+    textLen = len(f'{name} {surname}') / 2
+    x = 345 #Оффсет для текста
+    #Устанавливаем ФИ получателя
+    texting.text((x - (16 * textLen), 265), f'{name} {surname}', font=bigFont, fill = '#000000')
 
-    #Прочее(Срок действия, марки)
-    texting.text((x + 170, 85), 'Начало действия', font=smallFont, fill = '#000000')
-    texting.text((x + 170, 140), 'Истекает до', font=smallFont, fill = '#000000')
-    texting.text((50, 355), 'Имя', font=smallFont, fill = '#000000')
-    #Устанавливаем данные под заголовками
-    texting.text((x + 170, 104), dataStart, font=smallFont, fill = '#000000')
-    texting.text((x + 170, 158), dataEnd, font=smallFont, fill = '#000000')
+    #Устанавливаем id сертификата
+    texting.text((x + 170, 100), f'№ C{usr_id}', font=smallFont, fill = '#000000')
+    
+    #Устанавливаем дату выдачи сертификата
+    texting.text((x - 15, 440), f'{GetCurrentDate()}', font=smallFont, fill = '#000')
 
     #Сохраняем готовую карточку
     background.save(directory + 'out.png')
+
+def GetCurrentDate():
+    import datetime
+    date = str(datetime.datetime.today().date()).split('-')
+    return f'{date[2]}.{date[1]}.{date[0]}'
--- vk-bot/vk.py
+++ vk-bot/vk.py
@@ -20,12 +20,12 @@ def StartAgain():
 
 #объявление глобальных переменных
 vk = vk_api.VkApi(token=group_token)
-try:
-    vk._auth_token()
-    vk.get_api()
-    longpoll = VkBotLongPoll(vk, 198871990)
-except:
-    StartAgain()
+
+vk._auth_token()
+vk.get_api()
+longpoll = VkBotLongPoll(vk, 184139426)
+#except:
+#    StartAgain()
 
 path = os.path.abspath(__file__)
 path = path[0:-5] #директория папки данного бота
@@ -43,7 +43,7 @@ def senderAttach(peer_id, text, attachment):
 
 #получение информации о пользователе
 def getInfo(id):
-    userInfo = vk.method('users.get',{'user_ids': id, 'fields': 'photo_200'})
+    userInfo = vk.method('users.get',{'user_ids': id, 'fields': 'photo_200','lang':'ru'})
     info = [str(userInfo[0]['photo_200']),
                 str(userInfo[0]['first_name']),
                 str(userInfo[0]['last_name']),
@@ -51,21 +51,13 @@ def getInfo(id):
                 ]
     return info
 
-#скачивание по ссылке
-def download_avatar(url):
-    r = requests.get(url)
-    with open(file_link, "wb") as code:
-        code.write(r.content)
-    print(file_link)
-    return file_link
-
 #отправка фотокарточки
 def send_card(user_id):
     a = vk.method("photos.getMessagesUploadServer")
     b = requests.post(a['upload_url'], files={'photo': open(res_link, 'rb')}).json()
     c = vk.method('photos.saveMessagesPhoto', {'photo': b['photo'], 'server': b['server'], 'hash': b['hash']})[0]
     photo = f'photo{c["owner_id"]}_{c["id"]}'
-    vk.method("messages.send", {"peer_id": user_id, "message": "Держи", "attachment": photo,"random_id": 0})
+    vk.method("messages.send", {"peer_id": user_id, "attachment": photo,"random_id": 0})
 
 #команды доступные в личных сообщениях
 def directMessage(event):
@@ -73,25 +65,23 @@ def directMessage(event):
     from_id = event.object.from_id
     peer_id = event.object.peer_id
 
-    if event.object.text.lower() == "привет":
-        vk.method("messages.send", {"user_id": event.object.peer_id, "message": event.object.text, "random_id": 0})
-        return
-
-    if(msg.startswith(('карточка', 'карточку','картонку')) or 'сертификат' in msg):
-        dataStart = '01.09.2020'
-        dataEnd = '01.12.2020'
+    if('сертификат' == msg):
         info = getInfo(from_id)
-        download_avatar(info[0])
-        cc.create(info[1], info[2], info[3], dataStart, dataEnd,card_link)
+        cc.create(info[1], info[2], info[3])
         send_card(peer_id)
 
-try:
-    while True:
-        #прослушивание событий, то есть новых сообщений
-        for event in longpoll.listen():
-            if event.type == VkBotEventType.MESSAGE_NEW:
-                if event.object.peer_id == event.object.from_id or event.object.peer_id == 2000000001:
-                    directMessage(event)
+#try:
+#    while True:
+#        #прослушивание событий, то есть новых сообщений
+#        for event in longpoll.listen():
+#            if event.type == VkBotEventType.MESSAGE_NEW:
+#                if event.object.peer_id == event.object.from_id:
+#                    directMessage(event)
+#
+#except Exception as ex:
+#    StartAgain()
 
-except Exception as ex:
-    StartAgain()
+for event in longpoll.listen():
+    if event.type == VkBotEventType.MESSAGE_NEW:
+        if event.object.peer_id == event.object.from_id:
+            directMessage(event)
